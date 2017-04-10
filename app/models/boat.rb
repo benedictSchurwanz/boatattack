ceddea0a6cb62bf98babffd1d1883664642a266c
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-10 00:15:34 -0500

    initialize boat with name and length, with tests

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 class Boat
-	
+	attr_reader :name, :length
+
+	def initialize(args)
+		@length = args[:length]
+		@name = boat_namer
+	end
+
+	private
+
+	def boat_namer
+		# creating an array such that the index corresponds to the length of the boat
+		names = [0, "submarine", "destroyer", "cruiser", "battleship", "carrier"]
+
+		names[@length]
+	end
 end
---
+++ spec/boat_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../app/models/boat'
+
+describe 'Boat' do 
+	let(:test_boat) { Boat.new(length: 3) }
+
+	it 'is a boat object' do 
+		expect(test_boat).not_to eq nil
+	end
+
+	it 'has a name (type of boat)' do 
+		expect(test_boat.name).to eq "cruiser"
+	end
+
+	it 'has a length' do 
+		expect(test_boat.length.class).to eq Integer
+	end
+
+	it 'knows how many times it has been hit' do 
+
+	end
+
+	it 'has a sunk status' do 
+
+	end
+
+	xit 'knows its location on the board' do 
+
+	end
+end
