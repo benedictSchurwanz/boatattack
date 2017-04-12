0b04abfc9f2f0808afcc1134c7f42c9d0f839cee
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-12 02:10:19 -0500

    add game model

---
+++ spec/game_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../app/models/game'
+require_relative '../app/models/boat'
+require_relative '../app/models/cell'
+require_relative '../app/models/player'
+
+describe 'game setup' do 
+	let(:human_player) { Player.new(type: :human) }
+	let(:computer_player) { Player.new(type: :computer) }
+
+	context 'player initialization' do 
+		it 'can create a human player' do 
+			expect(human_player.type).to eq :human
+		end
+
+		it 'can create a computer player' do 
+			expect(computer_player.type).to eq :computer
+		end
+	end
+
+	context 'board initialization' do 
+
+	end
+end
