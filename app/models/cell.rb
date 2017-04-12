04e5cf2967a5f2413466d1aeced36fb21d19e370
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-12 00:51:16 -0500

    add notes describing function of cell instance variables

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -3,7 +3,11 @@ class Cell
 	attr_accessor :boat
 
 	def initialize
+		# hit status will be '0' for not-yet-attacked, 
+		# 'X' for hit and '/' for missed
 		@hit_status = '0'
+		# boat variable will hold the boat object that is in that spot
+		# boat objects will hold an array of cell objects it contains
 		@boat = 'empty'
 	end
 end
