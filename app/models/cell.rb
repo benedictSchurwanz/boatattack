dbf30af70cccef06dc7506569ca42ea2a84d0c6c
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-12 00:49:07 -0500

    add hit_status and boat presence attributes to cell class

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,3 +1,9 @@
 class Cell
-	
+	attr_reader :hit_status
+	attr_accessor :boat
+
+	def initialize
+		@hit_status = '0'
+		@boat = 'empty'
+	end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,7 +3,11 @@ require_relative '../app/models/cell'
 describe 'cell' do 
 	let(:test_cell) { Cell.new }
 
-	xit 'knows whether its been hit, starts out open' do 
-		expect(test_cell.)
+	it 'knows whether its been hit, starts out open' do 
+		expect(test_cell.hit_status).to eq '0'
+	end
+
+	it 'knows whether it holds part of a boat' do 
+		expect(test_cell.boat).to eq 'empty'
 	end
 end
