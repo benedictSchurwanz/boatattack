9319302369141280e220561ada4a1ef421fb4e3a
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-13 00:56:58 -0500

    add player creation to game initialization

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,1 +1,12 @@
+require_relative 'player'
+require_relative 'boat'
+require_relative 'cell'
+require_relative '../helpers/helpers'
 
+class Game
+	attr_reader :player
+
+	def initialize(options = {})
+		@player = {one: Player.new(type: :human), two: Player.new(type: :computer)}
+	end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,7 @@ require_relative '../app/models/game'
 require_relative '../app/models/boat'
 require_relative '../app/models/cell'
 require_relative '../app/models/player'
+require_relative '../app/helpers/helpers'
 
 describe 'game setup' do 
 	let(:human_player) { Player.new(type: :human) }
@@ -14,6 +15,19 @@ describe 'game setup' do
 
 		it 'can create a computer player' do 
 			expect(computer_player.type).to eq :computer
+		end
+
+		it 'creating a Game creates players' do 
+			game = Game.new
+
+			expect(game.player[:one].type).to eq :human
+			expect(game.player[:two].type).to eq :computer
+		end
+	end
+
+	context 'random boat placement' do 
+		it 'can pick a random spot on the board' do 
+			expect(computer_player.board[rand_row][rand_col].boat).to eq 'empty'
 		end
 	end
 end
