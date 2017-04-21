29ad7e68b605df6bb2fc22d5b70a1dd0f5a3da60
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-21 01:14:11 -0500

    refactor Boat model initialization

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,17 +3,23 @@ class Boat
 	attr_accessor :cells
 
 	def initialize(args)
-		@length = args[:length]
-		@name = boat_namer
-		@hit_points = args[:length]
-		@cells = []
+		if args[:length] do 
+			@length = args[:length]
+			@name = boat_namer
+		elsif args[:name] do 
+			@name = args[:name]
+			@length = boat_length
+		end
+
+		@hit_points = @length
+		@cells_occupied = []
 	end
 
 	# pass in as arguments the list of cells that this boat will occupy
 	# TBI: verify that the number of cells is equal to the boat length
 	def place_boat_in(*args)
 		args.each do |cell|
-			@cells << cell
+			@cells_occupied << cell
 		end
 	end
 
@@ -41,5 +47,11 @@ class Boat
 		names = [nil, "submarine", "destroyer", "cruiser", "battleship", "carrier"]
 
 		names[@length]
+	end
+
+	def boat_length
+		lengths = {"carrier" => 5, "battleship" => 4, "cruiser" => 3, "destroyer" => 2, "submarine" => 1}
+
+		lengths[@name]
 	end
 end
