88abc03f53b7d63abcb9d7fbff654e474cc70905
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-21 01:20:10 -0500

    refactor random row and column methods into Board class

--- app/helpers/rand_helpers.rb
+++ app/helpers/rand_helpers.rb
@@ -1,11 +1,3 @@
-def rand_row
-	rand(10) + 1
-end
-
-def rand_col
-	[:a, :b, :c, :d, :e, :f, :g, :h, :i, :j][rand(10)]
-end
-
 def rand_dir
-	["vertical", "horizontal"][rand(2)]
+	["vertical", "horizontal"].sample
 end
