fb4cd0397d540d38b31fec6c4e6617e6d7bfddd8
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-22 22:35:26 -0500

    add fleet attribute with generator method

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,11 +4,25 @@ require_relative '../helpers/boat_helpers'
 
 class Player
 	attr_accessor :board
-	attr_reader :type, :name
+	attr_reader :type, :fleet, :name
 
 	def initialize(options = {})
 		@board = Board.new
 		@name = options[:name]
 		@type = options[:type] # human or computer
+		@fleet = generate_fleet(options[:lengths])
+		@fleet ||= generate_fleet(bednar_fleet_lengths)
+	end
+	
+	private
+
+	def generate_fleet(lengths)
+		fleet = []
+
+		lengths.each do |boat_length|
+			fleet << Boat.new(length: boat_length, player: self)
+		end
+
+		fleet
 	end
 end
