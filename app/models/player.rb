67f1b8cc78d7ff64195c54c14cdd383dc156ca09
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-22 22:33:12 -0500

    refactor Player class

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,15 +1,13 @@
+require_relative 'board'
 require_relative 'boat'
-require_relative 'cell'
+require_relative '../helpers/boat_helpers'
 
 class Player
-	# attr_accessor :board
-	attr_reader :type, :boats
+	attr_accessor :board
+	attr_reader :type
 
 	def initialize(options = {})
 		@board = Board.new
-		@type = options[:type]
+		@type = options[:type] # human or computer
 	end
-
-	private
-	
 end
