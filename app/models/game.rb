8a0387b636af955385f0a44fe526a444ecdacc7c
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-22 22:12:40 -0500

    refactor to set bednar as default length, if length not passed in via options

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,7 +8,7 @@ class Game
 	attr_reader :players
 
 	def initialize(options = {})
-		lengths = bednar_fleet_lengths
+		lengths = options[:lengths] ||= bednar_fleet_lengths
 
 		@players = {one: Player.new(name: "One", type: :human, lengths: lengths), two: Player.new(name: "Two", type: :computer, lengths: lengths)}
 	end
