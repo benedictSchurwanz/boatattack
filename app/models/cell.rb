2ae5432d3d65d2afa0ab73391da9a783566c4074
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-22 23:09:41 -0500

    change status flags from strings to symbols

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -3,11 +3,11 @@ class Cell
 
 
 	def initialize(x, y)
-		# hit status will be 'open', 'hit', or 'miss'
-		@hit_status = 'open'
+		# hit status will be :open, :hit, or :miss
+		@hit_status = :open
 		# boat variable will hold the boat object that is in that spot
 		# boat objects will hold an array of cell objects it sits on
-		@boat = 'empty'
+		@boat = :empty
 
 		@x = x
 		@y = y
@@ -18,11 +18,11 @@ class Cell
 	end
 
 	def hit
-		@hit_status = 'hit'
+		@hit_status = :hit
 		@boat.hit
 	end
 
 	def miss
-		@hit_status = 'miss'
+		@hit_status = :miss
 	end
 end
