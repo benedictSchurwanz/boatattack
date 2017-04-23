489ee68dc536e00c0d32b35e5bad2cd53ab29766
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-22 23:05:54 -0500

    adjust comments to match different statuses

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -3,8 +3,7 @@ class Cell
 	attr_accessor :boat
 
 	def initialize(x, y)
-		# hit status will be '0' for not-yet-attacked, 
-		# 'X' for hit and '/' for missed
+		# hit status will be 'open', 'hit', or 'miss'
 		@hit_status = 'open'
 		# boat variable will hold the boat object that is in that spot
 		# boat objects will hold an array of cell objects it sits on
