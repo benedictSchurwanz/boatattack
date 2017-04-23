f6463ef7fd8e274053640daf86e5ba8cbdecbb4c
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-23 12:32:55 -0500

    refactor battleship test to reflect new fleet structure

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,7 +8,8 @@ describe 'Player' do
 	end
 
 	it 'has a battleship' do 
-		expect(player.fleet[:battleship].length).to eq 4
+		battleship = player.fleet.find { |boat| boat.name == "battleship" }
+		expect(battleship.length).to eq 4
 	end
 
 	it 'knows how many boats are still afloat' do 
