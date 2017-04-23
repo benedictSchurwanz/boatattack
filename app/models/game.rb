e55afe0adc4e96a371bbc5f9667b09d2f6b57480
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-22 22:24:30 -0500

    refactor lengths to use options or default Bednar values

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,14 +1,12 @@
 require_relative 'player'
-require_relative 'boat'
-require_relative 'cell'
-require_relative '../helpers/rand_helpers'
 require_relative '../helpers/boat_helpers'
 
 class Game
 	attr_reader :players
 
 	def initialize(options = {})
-		lengths = options[:lengths] ||= bednar_fleet_lengths
+		lengths = options[:lengths] 
+		lengths ||= bednar_fleet_lengths
 
 		@players = {one: Player.new(name: "One", type: :human, lengths: lengths), two: Player.new(name: "Two", type: :computer, lengths: lengths)}
 	end
