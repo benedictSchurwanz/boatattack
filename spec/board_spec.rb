1ece407f7682940a50685450f598e7ab0cc8fff0
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-23 00:31:47 -0500

    move board tests from player tests to new board class tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,19 @@
+require_relative '../app/models/board'
+require_relative '../app/models/cell'
 
+describe Board do 
+	let(:board) { Board.new }
+
+	context 'blank board generation' do 
+		it 'board has 10 rows and 10 columns' do 
+			expect(board.cell_at(0,0)).to be_truthy
+			expect(board.cell_at(9,9)).to be_truthy
+			expect(board.cell_at(9,10)).to eq nil
+			# expect(board.cell_at(10,9)).to raise_error(NoMethodError)
+		end
+		
+		it 'board contains cells' do 
+			expect(board.cell_at(1, 1).class).to eq Cell
+		end
+	end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,26 +1,5 @@
 require_relative '../app/models/player'
 
-describe 'board initialization' do 
-	let(:player_1) { Player.new }
-
-	it 'has a board' do 
-		expect(player_1.board).to be_truthy
-	end
-
-	it 'has a board with 10 rows' do 
-		expect(player_1.board.length).to eq 10
-	end
-
-	it 'has a board with 10 columns' do 
-		expect(player_1.board[1].length).to eq 10
-	end
-
-	it 'has cells that can be referenced by coordinates' do 
-		expect(player_1.board[1][:a].hit_status).to eq 'open'
-		expect(player_1.board[10][:j].hit_status).to eq 'open'
-	end
-end
-
 describe 'Player' do 
 	let(:player) { Player.new }
 
