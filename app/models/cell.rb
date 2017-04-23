6b010194ee9f41ac8076966e623247fbfd689131
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-22 23:05:15 -0500

    add coordinate attributes

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -2,12 +2,15 @@ class Cell
 	attr_accessor :hit_status
 	attr_accessor :boat
 
-	def initialize
+	def initialize(x, y)
 		# hit status will be '0' for not-yet-attacked, 
 		# 'X' for hit and '/' for missed
 		@hit_status = 'open'
 		# boat variable will hold the boat object that is in that spot
-		# boat objects will hold an array of cell objects it contains
+		# boat objects will hold an array of cell objects it sits on
 		@boat = 'empty'
+
+		@x = x
+		@y = y
 	end
 end
