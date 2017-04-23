3f3f405effe0f950a6ef385b74af6f060d6d2413
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-22 22:05:19 -0500

    initialize Game with two players, one human one computer

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,6 +5,11 @@ require_relative '../helpers/rand_helpers'
 require_relative '../helpers/boat_helpers'
 
 class Game
-	def initialize()
+	attr_reader :players
+
+	def initialize(options = {})
+		lengths = bednar_fleet_lengths
+
+		@players = {one: Player.new(name: "One", type: :human, lengths: lengths), two: Player.new(name: "Two", type: :computer, lengths: lengths)}
 	end
 end
