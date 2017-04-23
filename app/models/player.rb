45165bd17f188536651fc9e6507679fa7d92a05d
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-22 22:34:11 -0500

    add name field to Player class

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,10 +4,11 @@ require_relative '../helpers/boat_helpers'
 
 class Player
 	attr_accessor :board
-	attr_reader :type
+	attr_reader :type, :name
 
 	def initialize(options = {})
 		@board = Board.new
+		@name = options[:name]
 		@type = options[:type] # human or computer
 	end
 end
