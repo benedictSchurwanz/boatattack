1d5149c33519244476b56c84920ddb69dff5f1a3
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-24 00:27:42 -0500

    add tests for random cell return methods

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../app/models/board'
+require_relative '../app/models/boat'
 require_relative '../app/models/cell'
 
 describe Board do 
@@ -13,12 +14,36 @@ describe Board do
 		it 'board has 10 rows and 10 columns' do 
 			expect(board.cell_at(0,0)).to be_truthy
 			expect(board.cell_at(9,9)).to be_truthy
-			expect(board.cell_at(9,10)).to eq nil
+			expect(board.cell_at(9,10)).to be_falsey
 			# expect(board.cell_at(10,9)).to raise_error(NoMethodError)
 		end
 		
 		it 'board contains cells' do 
-			expect(board.cell_at(1, 1).class).to eq Cell
+			expect(board.cell_at(1, 1)).to be_a Cell
+		end
+	end
+
+	context 'random cells:' do 
+		it 'can return a random cell' do 
+			expect(board.random_cell).to be_a Cell
+		end
+
+		it 'can return a random empty cell' do 
+			empty_cell = board.random_empty_cell
+
+			expect(empty_cell).to be_a Cell
+			expect(empty_cell.boat).to eq :empty
+		end
+
+		it 'can return a random occupied cell' do 
+			submarine = Boat.new(length: 1)
+			submarine.occupy([board.cell_at(1,1)])
+			board.cell_at(1,1).occupied_by(submarine)
+
+			occupied_cell = board.random_occupied_cell
+
+			expect(occupied_cell).to eq board.cell_at(1,1)
+			expect(occupied_cell.boat).to eq submarine
 		end
 	end
 end
