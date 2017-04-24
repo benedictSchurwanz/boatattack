452e47959ddf4fb89139feb3b5b30381658a2be7
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-04-24 00:36:38 -0500

    add tests for name, type, default fleet

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,15 @@
 require_relative '../app/models/player'
 
 describe 'Player' do 
-	let(:player) { Player.new }
+	let(:player) { Player.new(name: "Joe", type: :human) }
+
+	it 'has a name' do 
+		expect(player.name).to eq 'Joe'
+	end
+
+	it 'has a type' do 
+		expect(player.type).to eq :human
+	end
 
 	it 'has boats' do 
 		expect(player.fleet[0]).to be_a Boat
@@ -9,7 +17,7 @@ describe 'Player' do
 
 	it 'has a battleship' do 
 		battleship = player.fleet.find { |boat| boat.name == "battleship" }
-		
+
 		expect(battleship.length).to eq 4
 	end
 
@@ -19,5 +27,16 @@ describe 'Player' do
 
 	it 'knows how many shots it can fire this turn' do 
 		expect(player.volley).to eq player.boats_remaining
+	end
+
+	it 'defaults to Bednar fleet' do 
+		expect(player.fleet[0].length).to eq 5
+		expect(player.fleet[1].length).to eq 4
+		expect(player.fleet[2].length).to eq 3
+		expect(player.fleet[3].length).to eq 2
+		expect(player.fleet[4].length).to eq 2
+		expect(player.fleet[5].length).to eq 1
+		expect(player.fleet[6].length).to eq 1
+		expect(player.fleet[7]).to be_falsey
 	end
 end
