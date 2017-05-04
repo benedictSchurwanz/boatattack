7e1575bcf3fddfbe00247d0b421f104b450bf3c0
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2017-05-04 00:47:49 -0500

    add test driver code to format board display

--- runner.rb
+++ runner.rb
@@ -13,12 +13,25 @@ game.setup
 
 # puts game.players[:one].fleet[0].cells
 
-game.players[:one].fleet.each do |boat|
-	puts boat.name
+# game.players[:one].fleet.each do |boat|
+# 	puts boat.name
 	
-	boat.cells.each do |cell|
-		puts "(#{cell.row}, #{cell.col})\n"
-	end
+# 	boat.cells.each do |cell|
+# 		puts "(#{cell.row}, #{cell.col})\n"
+# 	end
 
-	puts "\n"
-end
+# 	puts "\n"
+# end
+
+## fires at all cells on player one's board and prints that board
+# player = game.players[:one]
+
+# 10.times do |row|
+# 	10.times do |col|
+# 		target = player.board.cell_at(row, col)
+
+# 		game.fire_on(target) # fire_on method temporarily made public for testing
+# 	end
+# end
+
+# game.print_current_board(game.players[:one].board)
